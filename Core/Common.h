c5a75794741b9568d34193b05e6fe19098e0b966
etsai <thegod0fde4th@gmail.com>
2012-12-12 01:15:54 -0800

    Created header that contains common variables and consts

---
+++ Core/Common.h
@@ -0,0 +1,12 @@
+#ifndef ETSAI_PHOENIX_CORE_COMMON
+#define ETSAI_PHOENIX_CORE_COMMON
+
+namespace etsai {
+namespace phoenix {
+
+const double PI= 3.141592653589793238462643383279502;
+
+}   //namespace phoenix
+}   //namespace etsai
+
+#endif
